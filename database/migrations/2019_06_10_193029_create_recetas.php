7fbb23c7bf91d1a19b014ddc117a6c1cb6c27146
Andres <lezzzca@gmail.com>
2019-06-10 17:22:56 -0300

    agregadas todas las migraciones de las tablas de base de datos

---
+++ database/migrations/2019_06_10_193029_create_recetas.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecetas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recetas', function (Blueprint $table) {
+            $table->bigIncrements('id_recetas');
+            $table->string('titulo');
+            $table->string('categoria');
+            $table->string('imagen');
+            $table->longText('preparacion');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recetas');
+    }
+}
---
+++ database/migrations/2019_06_10_201535_add_foreign_key_recetas.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignKeyRecetas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('recetas', function (Blueprint $table) {
+            $table->bigInteger('fk_autor')->unsigned();
+            $table->foreign('fk_autor')
+                ->references('id_autor')->on('autores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('recetas', function (Blueprint $table) {
+            $table->dropForeign('recetas_autores_id_autor_foreign');
+        });
+    }
+}
